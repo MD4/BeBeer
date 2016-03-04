80c5e3bd3757918f8f99170ee1b6774b6b3b247d
FXHibon <hibonfx@gmail.com>
2016-03-04 11:00:57 +0100

    Link user rate to beer profile

--- app/src/main/java/android/epsi/com/bebeer/activities/user/profile/adapters/UserRatingsAdapter.java
+++ app/src/main/java/android/epsi/com/bebeer/activities/user/profile/adapters/UserRatingsAdapter.java
@@ -3,6 +3,7 @@ package android.epsi.com.bebeer.activities.user.profile.adapters;
 import android.content.Context;
 import android.epsi.com.bebeer.R;
 import android.epsi.com.bebeer.bean.Rating;
+import android.epsi.com.bebeer.services.remote.ApiClient;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +18,11 @@ import java.util.List;
  */
 public class UserRatingsAdapter extends ArrayAdapter<Rating> {
 
+    private final ApiClient mApiClient;
+
     public UserRatingsAdapter(Context context, int resource, List<Rating> objects) {
         super(context, resource, objects);
+        mApiClient = new ApiClient(context);
     }
 
     @Override
@@ -26,12 +30,25 @@ public class UserRatingsAdapter extends ArrayAdapter<Rating> {
         if (convertView == null) {
             convertView = View.inflate(getContext(), R.layout.user_profile_rating_item, null);
         }
-
         Rating item = getItem(position);
+        final TextView beerId = (TextView) convertView.findViewById(R.id.user_profile_rating_item_beer_id);
+        final TextView date = (TextView) convertView.findViewById(R.id.user_profile_rating_item_date);
+        final RatingBar ratingBar = (RatingBar) convertView.findViewById(R.id.user_profile_rating_item_rate);
 
-        TextView beerId = (TextView) convertView.findViewById(R.id.user_profile_rating_item_beer_id);
-        TextView date = (TextView) convertView.findViewById(R.id.user_profile_rating_item_date);
-        RatingBar ratingBar = (RatingBar) convertView.findViewById(R.id.user_profile_rating_item_rate);
+        // TODO it create problems
+//        mApiClient.getBeer(item.getBeerId()).enqueue(new Callback<Beer>() {
+//            @Override
+//            public void onResponse(Response<Beer> response, Retrofit retrofit) {
+//                if (response.isSuccess()) {
+//                    beerId.setText(response.body().getName());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable t) {
+//
+//            }
+//        });
 
         beerId.setText(item.getBeerId());
         date.setText(DateUtils.getRelativeTimeSpanString(item.getDate().getMillis()));
